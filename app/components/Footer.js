45d670b6cd5c492575afd123313d143a6fe69cfa
pirogue.one <alyona.evg@yandex.ru>
2025-02-05 12:44:53 +0300

    small changes

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,4 +1,12 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+
+
 export default function Footer() {
+
+  const t = useTranslations('Footer');
+
   return (
     <footer className="bg-gray-800 text-white p-4 mt-8">
       <div className="container mx-auto text-center">
---
+++ app/feedback/page.js
@@ -0,0 +1,17 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+
+
+export default function FeedbackForm() {
+
+    const t = useTranslations('FeedbackForm');
+  
+    return (
+      <form>
+
+
+      </form>
+    )
+  }
+  
